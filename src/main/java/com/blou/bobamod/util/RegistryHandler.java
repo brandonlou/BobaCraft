7a486f98cd5d77deb56d17abd395905d7f263c59
Brandon Lou <14984696+brandonlou@users.noreply.github.com>
2020-08-19 20:22:24 -0700

    Add thai tea

--- src/main/java/com/blou/bobamod/util/RegistryHandler.java
+++ src/main/java/com/blou/bobamod/util/RegistryHandler.java
@@ -2,6 +2,7 @@ package com.blou.bobamod.util;
 
 import com.blou.bobamod.Boba;
 import com.blou.bobamod.items.ItemBase;
+import com.blou.bobamod.items.ThaiTea;
 import net.minecraft.item.Item;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -18,5 +19,6 @@ public class RegistryHandler {
 
     // Items
     public static final RegistryObject<Item> BLACK_TEA = ITEMS.register("black_tea", ItemBase::new);
+    public static final RegistryObject<ThaiTea> THAI_TEA = ITEMS.register("thai_tea", ThaiTea::new);
 
 }
