61c00efd3cb54f7902a4dd0c79b63921b28ff1db
Brandon Lou <14984696+brandonlou@users.noreply.github.com>
2020-08-19 18:39:15 -0700

    Add black tea item

---
+++ README.md
@@ -0,0 +1,9 @@
+# The Boba Mod
+
+> Make boba in minecraft!
+
+## Setup
+Open up `IntelliJ`. Import project by selecting `build.gradle`.
+After it loads, run `./gradlew genIntellijRuns`.
+
+Test using `./gradlew runClient`
---
+++ src/main/java/com/blou/bobamod/util/RegistryHandler.java
@@ -0,0 +1,22 @@
+package com.blou.bobamod.util;
+
+import com.blou.bobamod.Boba;
+import com.blou.bobamod.items.ItemBase;
+import net.minecraft.item.Item;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class RegistryHandler {
+
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, Boba.MOD_ID);
+
+    public static void init() {
+        ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
+    }
+
+    // Items
+    public static final RegistryObject<Item> BLACK_TEA = ITEMS.register("black_tea", ItemBase::new);
+
+}
