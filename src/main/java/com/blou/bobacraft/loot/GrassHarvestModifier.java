5af6284b16c03fbf248604bc622390f44388cd59
Brandon Lou <14984696+brandonlou@users.noreply.github.com>
2020-09-14 20:14:36 -0700

    Add naturally occurring cassava seed drops

--- src/main/java/com/blou/bobacraft/loot/GrassHarvestModifier.java
+++ src/main/java/com/blou/bobacraft/loot/GrassHarvestModifier.java
@@ -1,12 +1,14 @@
 package com.blou.bobacraft.loot;
 
-import com.blou.bobacraft.BobaCraft;
 import com.google.gson.JsonObject;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.loot.LootContext;
+import net.minecraft.loot.LootParameters;
 import net.minecraft.loot.conditions.ILootCondition;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.biome.Biome;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.loot.GlobalLootModifierSerializer;
 import net.minecraftforge.common.loot.LootModifier;
@@ -16,8 +18,9 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.registries.ForgeRegistries;
 
 import javax.annotation.Nonnull;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+import java.util.Random;
 
 @Mod.EventBusSubscriber(modid="bobacraft", bus=Mod.EventBusSubscriber.Bus.MOD, value=Dist.CLIENT)
 public class GrassHarvestModifier extends LootModifier {
@@ -38,11 +41,18 @@ public class GrassHarvestModifier extends LootModifier {
     @Nonnull
     protected List<ItemStack> doApply(List<ItemStack> generatedLoot, LootContext context) {
 
-        List<ItemStack> newLoot = new ArrayList<>();
-        Item cassavaSeedItem = ForgeRegistries.ITEMS.getValue(new ResourceLocation("bobacraft:cassava_seed_item"));
-        newLoot.add(new ItemStack(cassavaSeedItem, 1));
+        BlockPos blockPos = new BlockPos(Objects.requireNonNull(context.get(LootParameters.field_237457_g_)));
+        Biome biome = context.getWorld().getBiome(blockPos);
 
-        return newLoot;
+        // 1/20 or 5% chance of dropping a cassava seed if in jungle.
+        if(biome.getCategory() == Biome.Category.JUNGLE && new Random().nextInt(20) == 0) {
+
+            Item cassavaSeedItem = ForgeRegistries.ITEMS.getValue(new ResourceLocation("bobacraft:cassava_seed_item"));
+            generatedLoot.add(new ItemStack(cassavaSeedItem, 1));
+
+        }
+
+        return generatedLoot;
 
     }
 
@@ -59,21 +69,5 @@ public class GrassHarvestModifier extends LootModifier {
         }
 
     }
-
-    //    @SubscribeEvent
-//    public static void onBreakGrassInJungle(BlockEvent.BreakEvent event) {
-//
-//        Block currentBlock = event.getState().getBlock();
-//        Biome currentBiome = event.getWorld().getBiome(event.getPos());
-//
-//        boolean isGrassBlock = (currentBlock == Blocks.TALL_GRASS);
-//        boolean inJungle = (currentBiome == ForgeRegistries.BIOMES.getValue(Biomes.JUNGLE.getRegistryName()));
-//
-//        if(isGrassBlock && inJungle) {
-////            ItemStack stack =
-////            currentBlock.spawnAdditionalDrops(event.getState(), , event.getPos(), stack);
-//        }
-//
-//    }
 
 }
